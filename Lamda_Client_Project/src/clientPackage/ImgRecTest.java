af7ddad7e64e476115765abad99301af18496a61
jfahrens124 <jfahrens124@gmail.com>
2015-05-11 10:41:48 -0400

    Update ImgRecTest.java

--- Lamda_Client_Project/src/clientPackage/ImgRecTest.java
+++ Lamda_Client_Project/src/clientPackage/ImgRecTest.java
@@ -46,11 +46,11 @@ public class ImgRecTest {
 	
 	public static void fillSquare(BufferedImage i, int x, int y, int w, int h, Color c) {
 		for (int n = x; n < w; n++) {
-			for (int m = y; m < h; y++) {
+			for (int m = y; m < h; m++) {
 				i.setRGB(n, m, c.getRGB());
 			}
 		}
-	}
+	} 
 
 	public static boolean sameColor(Color a, Color b, int e) {
 		double d = Math.sqrt(Math.pow(b.getRed() - a.getRed(), 2)
