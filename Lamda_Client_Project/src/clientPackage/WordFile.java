c97371899d40328244004efb15d9aacf601b9777
andrewml64 <andrew.lent99@gmail.com>
2015-05-28 23:39:26 -0400

    Update WordFile.java
    Commented and ready to go

--- Lamda_Client_Project/src/clientPackage/WordFile.java
+++ Lamda_Client_Project/src/clientPackage/WordFile.java
@@ -6,23 +6,34 @@ import java.io.FileOutputStream;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;   
 
+
 public class WordFile {
 	
 	private XWPFDocument doc;
 	private String name;
-	
+
+    /* This creates a Word document in the docx format or opens one if it already exits
+    * name is just the name of the file with no extension
+    */
     public WordFile(String n) throws IOException{   
           name = n;
           try{
+              //Creating an input stream allows a file to be updated rather than overwritten
               FileInputStream file = new FileInputStream("C:\\Users\\Andrew\\Desktop\\"+ name + ".docx");
         	  doc = new XWPFDocument(file);
           }catch (FileNotFoundException e){
         	  doc = new XWPFDocument();
           }
+          //Creating an output stream allows a file to be written to
           FileOutputStream fos = new FileOutputStream(new File("C:\\Users\\Andrew\\Desktop\\"+ name + ".docx"));   
           doc.write(fos);   
           fos.close();   
      }
+     
+     /*This function takes an image and puts into the created word document with the specified size
+     * path is the actual path to the image, including the name of the image
+     * This function only works with JPEG images
+     */
      public void writeImg(String path, int l, int w) throws InvalidFormatException, IOException{
     	 
     	 FileInputStream file = new FileInputStream("C:\\Users\\Andrew\\Desktop\\"+ name + ".docx");
